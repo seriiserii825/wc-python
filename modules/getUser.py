d56a613a35c6936c71f284edb8f83d7d24ec5156
serii burduja <seriiburduja@gmail.com>
2024-09-17 18:44:46 +0300

    feat: listProducts

--- modules/getUser.py
+++ modules/getUser.py
@@ -1,5 +1,5 @@
-import os
+import subprocess
 def getUser():
-    command = f"wp user list --field=name"
-    user = os.system(command)
+    command = f"wp user list --field=user_login --role=administrator"
+    user = subprocess.check_output(command, shell=True, text=True).strip()
     return user
--- modules/listProducts.py
+++ modules/listProducts.py
@@ -1,6 +1,14 @@
+import json
+import subprocess
 from modules.getUser import getUser
 
 
 def listProducts():
     user = getUser()
-    print(f"user: {user}")
+    command=f"wp wc product list --user={user} --format=json"
+    products = subprocess.check_output(command, shell=True, text=True)
+    products = json.loads(products)
+    for product in products:
+        print(product['name'])
+        print(product)
+
